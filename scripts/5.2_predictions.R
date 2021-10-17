468be1a178372e7d58adea7f7cb546e1eca4e86f
Erika Dvarionaite <43474174+ErikaDva@users.noreply.github.com>
2021-10-17 21:58:37 +0200

    Add files via upload

---
+++ scripts/5.2_predictions.R
@@ -0,0 +1,142 @@
+# ##############################################################################
+#
+##  Extract information from models & make predictions
+#
+# ##############################################################################
+
+# Setting working directory
+setwd("~/Desktop/crc_analysis/scripts") #macbook
+setwd("C:/Users/Erika Dvarionaite/iCloudDrive/Desktop/crc_analysis/scripts") #windows
+
+# Packages
+library(tidyverse)
+library(SIAMCAT)
+library(yaml)
+
+# ##############################################################################
+# general 
+
+memory.limit(56000)
+memory.size(TRUE)
+
+set.seed(2021)
+
+# ##############################################################################
+# Get data
+
+meta <- read_tsv(file = '../data/meta/meta.crc.2.tsv')
+# ##############################################################################
+# Parameters
+
+parameters <- yaml.load_file('../parameters.yaml')
+
+all.studies <- parameters$all.studies
+ada.studies <- parameters$ada.studies
+
+tag="level4ec"
+ml.method="lasso"
+stage="ADA"
+
+# ##############################################################################
+# Import models
+
+# ADA
+models <- list()
+for (study in ada.studies){
+  tag="eggnog"
+  stage="ADA"
+  ml.method="randomForest"
+  # import all models
+  mdl.path <- paste0("../models/", tag, "/", study, "_", stage, "_stage_", ml.method, "_model.RData")
+  load(mdl.path)
+  cat(tag, stage, 'model loaded for', study, '...\n')
+  models[[study]] <- siamcat
+}
+
+# CRC
+models <- list()
+for (study in all.studies){
+  
+  stage = "CRC"
+  # import all models
+  mdl.path <- paste0("../models/", tag, "/", study, "_", stage, "_stage_", ml.method, "_model2.RData")
+  load(mdl.path)
+  cat(tag, stage, 'model loaded for', study, '...\n')
+  models[[study]] <- siamcat
+}
+
+
+# ##############################################################################
+# Make predictions
+
+tag="species" # change
+ml.method = "lasso"
+
+fn.path <- paste0("../data/", tag, "/filtered_", tag, "_full.tsv")
+feat.all <- read.table(fn.path, 
+                       sep = "\t", stringsAsFactors = F, 
+                       header = T, check.names = F, 
+                       row.names = 1, quote ="", fill = F)
+cat(tag, 'feature table loaded...\n')
+
+if (tag == "species") {
+  fn.path <- paste0("../data/", tag, "/filtered.", tag, ".NEW.tsv")
+  feat.all <- read.table(fn.path, 
+                         sep = "\t", stringsAsFactors = F, 
+                         header = T, check.names = F, 
+                         row.names = 1, quote ="", fill = F)
+  cat(tag, 'feature table loaded...\n') 
+}
+
+models <- list()
+
+for (study in parameters$all.studies){
+  stage = "CRC"
+  # import all models
+  mdl.path <- paste0("../models/", tag, "/", study, "_", stage, "_stage_", ml.method, "_model_NEW.RData")
+  load(mdl.path)
+  cat(tag, stage, 'model loaded for', study, '...\n')
+  models[[study]] <- siamcat
+}
+
+pred.matrix <- matrix(NA, nrow=nrow(meta), 
+                      ncol=length(all.studies), 
+                      dimnames = list(meta$Sample_ID, 
+                                      all.studies))
+
+for (study in parameters$all.studies){
+  
+  # load model
+  siamcat <- models[[study]]
+  temp <- rowMeans(pred_matrix(siamcat))
+  pred.matrix[names(temp), study] <- temp
+  
+  # predict other studies
+  for (study_ext in setdiff(parameters$all.studies, study)){
+    
+    meta.test <- meta %>%
+      filter(Study == study_ext) %>%
+      filter(Group != "ADA")
+    
+    feat.test <- feat.all[,meta.test %>% pull(Sample_ID)]
+    
+    meta.test <- data.frame(meta.test)
+    rownames(meta.test) <- meta.test$Sample_ID
+    
+    siamcat.test <- siamcat(feat=feat.test,
+                            meta = meta.test, #added
+                            label = "Group", case = "CRC") #added
+    
+    siamcat.test <- make.predictions(siamcat, siamcat.holdout = siamcat.test)
+    
+    temp <- rowMeans(pred_matrix(siamcat.test))
+    pred.matrix[names(temp), study] <- temp
+    
+    cat("Predictions using", study, "model on", study_ext, 'data --> DONE\n')
+
+  }
+  write.table(pred.matrix, file=paste0('../files/',tag,'/predictions_', 
+                                         ml.method, '.tsv'), 
+              quote=FALSE, sep='\t', row.names=TRUE, col.names=TRUE)
+  cat("Predictions for ", tag, 'models complete...\n')
+}
