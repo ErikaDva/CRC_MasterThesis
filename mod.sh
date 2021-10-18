bc4db57aff013f26c8a3f499be6414b0d25ddcde
Erika Dvarionaite <43474174+ErikaDva@users.noreply.github.com>
2021-10-18 10:14:33 +0200

    Add files via upload

---
+++ mod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+module load metaphlan3/3.0.4-foss-2018a
+module load Bowtie2/2.3.4.1-foss-2018a
+module load Trim_Galore/0.6.5-foss-2018a-Python-3.6.4
+module load SAMtools/1.10-foss-2018a
+module load FastQC/0.11.7-Java-1.8.0_172
+module load MultiQC/1.7-foss-2018a-Python-2.7.14
