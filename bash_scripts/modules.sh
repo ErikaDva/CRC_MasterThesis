653cc2b65fa7803d7369c6f85fcad720c9ad5e1f
Erika Dvarionaite <43474174+ErikaDva@users.noreply.github.com>
2021-10-18 10:16:38 +0200

    Update modules.sh

--- bash_scripts/modules.sh
+++ bash_scripts/modules.sh
@@ -5,3 +5,4 @@ module load Trim_Galore/0.6.5-foss-2018a-Python-3.6.4
 module load SAMtools/1.10-foss-2018a
 module load FastQC/0.11.7-Java-1.8.0_172
 module load MultiQC/1.7-foss-2018a-Python-2.7.14
+module load HUManN/3.0.0a4-foss-2020b-Python-3.8.6
